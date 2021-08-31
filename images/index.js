85c144a9f6248fbb9bec0be136cd2cf2b1ac4080
Sebastian <sebgraph7@gmail.com>
2021-08-31 16:52:12 -0500

    Updates

---
+++ images/index.js
@@ -0,0 +1,33 @@
+const Images = {
+  logo: "../images/logos/logo.svg",
+  menu: {
+    productIcon: "../images/icons/courses.svg",
+    supportIcon: "../images/icons/tutorials.svg",
+    companyIcon: "../images/icons/account.svg",
+    hamburgerIcon: "../images/icons/hamburger.svg",
+  },
+  tooltip: {
+    profileIcon: "../images/icons/profile.svg",
+    settingsIcon: "../images/icons/settings.svg",
+    signoutIcon: "../images/icons/signout.svg",
+  },
+
+  buttons: {
+    purchaseRing: "../images/icons/icon-ring.svg",
+    purchaseIcon: "../images/icons/credit.svg",
+  },
+
+  mockups: {
+    mockup1: "../images/animations/mockup1.svg",
+    mockup2: "../images/animations/mockup2.svg",
+    mockup3: "../images/animations/mockup3.svg",
+    mockup4: "../images/animations/mockup4.svg",
+    mockup5: "../images/animations/mockup5.svg",
+  },
+
+  card: {
+    icon: "../images/icons/credit.svg",
+  },
+}
+
+export default Images
