011bc57763ab257e51e481834d667efcce3edfbf
Sebastian <sebgraph7@gmail.com>
2021-08-31 17:13:54 -0500

    Updates

--- images/index.js
+++ images/index.js
@@ -1,34 +1,53 @@
 import Logo from "./logos/logo.svg"
+import productIcon from "./icons/courses.svg"
+import supportIcon from "./icons/tutorials.svg"
+import companyIcon from "./icons/account.svg"
+import hamburgerIcon from "./icons/hamburger.svg"
+
+import profileIcon from "./icons/profile.svg"
+import settingsIcon from "./icons/settings.svg"
+import signoutIcon from "./icons/signout.svg"
+
+import purchaseRing from "./icons/icon-ring.svg"
+import purchaseIcon from "./icons/credit.svg"
+
+import mockup1 from "./animations/mockup1.svg"
+import mockup2 from "./animations/mockup2.svg"
+import mockup3 from "./animations/mockup3.svg"
+import mockup4 from "./animations/mockup4.svg"
+import mockup5 from "./animations/mockup5.svg"
+
+import iconCard from "./icons/credit.svg"
 
 const Images = {
   logo: Logo,
   menu: {
-    productIcon: "../images/icons/courses.svg",
-    supportIcon: "../images/icons/tutorials.svg",
-    companyIcon: "../images/icons/account.svg",
-    hamburgerIcon: "../images/icons/hamburger.svg",
+    productIcon: productIcon,
+    supportIcon: supportIcon,
+    companyIcon: companyIcon,
+    hamburgerIcon: hamburgerIcon,
   },
   tooltip: {
-    profileIcon: "../images/icons/profile.svg",
-    settingsIcon: "../images/icons/settings.svg",
-    signoutIcon: "../images/icons/signout.svg",
+    profileIcon: profileIcon,
+    settingsIcon: settingsIcon,
+    signoutIcon: signoutIcon,
   },
 
   buttons: {
-    purchaseRing: "../images/icons/icon-ring.svg",
-    purchaseIcon: "../images/icons/credit.svg",
+    purchaseRing: purchaseRing,
+    purchaseIcon: purchaseIcon,
   },
 
   mockups: {
-    mockup1: "../images/animations/mockup1.svg",
-    mockup2: "../images/animations/mockup2.svg",
-    mockup3: "../images/animations/mockup3.svg",
-    mockup4: "../images/animations/mockup4.svg",
-    mockup5: "../images/animations/mockup5.svg",
+    mockup1: mockup1,
+    mockup2: mockup2,
+    mockup3: mockup3,
+    mockup4: mockup4,
+    mockup5: mockup5,
   },
 
   card: {
-    icon: "../images/icons/credit.svg",
+    icon: iconCard,
   },
 }
 
